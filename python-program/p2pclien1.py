d6b8487d1fa312b36b6d934d03fae398165ebbee
Rohan Garg <rohangarg12@gmail.com>
2019-09-10 22:15:48 +0530

    changed to forking architecture - kinda working

--- python-program/p2pclien1.py
+++ python-program/p2pclien1.py
@@ -1,92 +1,64 @@
-# import socket programming library 
 import socket 
-
-# import thread module 
 from _thread import *
-import threading 
+import threading
+import os
+import time
 
-# print_lock = threading.Lock() 
+def threaded(c):
+	# print("hello") 
+	while True: 
+ 
+		data = c.recv(1024) 
+		if not data: 
+			print('Bye') 
+			break
 
-# thread fuction 
-def threaded_recv(s):
-	print("hello")
-	
-	# s.listen()
-	# while True:
-	# 	c, addr = s.accept
+		data = data[::-1] 
 
-	# 	print('Connected to :', addr[0], ':', addr[1])
-
-	# 	while True:
-	# 		data = c.recv(1024)
-	# 		if not data:
-	# 				break
-	# 		c.send(data)
-	# 	c.close()
-	# s.close()
-
-def threaded_send():
-	host = '127.0.0.1'
-
-	port = 7890
-
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	s.bind((host, 7891))
-	print("socket binded to port", 7891)
-
-	s.connect((host, port))
-	message = "client1"[::-1]
-	while True:
-		s.send(message.encode('ascii'))
-
-		data = s.recv(1024)
-
-		print('Recieved :', str(data.encode('ascii')))
-		ans = input('\nDo you want to send the message again? :')
-		if ans == 'y':
-				continue
-		else:
-				break
-	s.close()
-
+		c.send(data)
+	c.close()
 
 def Main():
-	# start_new_thread(threaded_send, ())
-	print("hello")
-	host = ""
+	if os.fork() == 0:
+		host = '127.0.0.1'
 
-	port = 4567
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host, port))
+		port = 7890
 
-	print("listeniing socket binded to port", port)
-	start_new_thread(threaded_recv, (s,)) 
-	# host = "" 
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		time.sleep(5)
+		s.connect((host, port))
+		message = "client1"[::-1]
+		while True:
+			s.send(message.encode('ascii'))
+
+			data = s.recv(1024)
+
+			print('Recieved :', str(data.decode('ascii')))
+			ans = input('\nDo you want to send the message again? :')
+			if ans == 'y':
+					continue
+			else:
+					break
+		s.close()
+		os._exit(0)
+	else:
+		host = ""
+		port = 4567
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind((host, port))
+
+		print("listeniing socket binded to port", port)
+
+		s.listen(5)
+		print("socket is listening")
+		while True:
+			c, addr = s.accept()
+
+			print('Connected to :', addr[0], ':', addr[1])
+
+			start_new_thread(threaded, (c,))
+		s.close()
 	
-	# port = 1547
-	# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	# s.bind((host, port)) 
-	# print("socket binded to port", port) 
-
-	# # put the socket into listening mode 
-	# s.listen(5) 
-	# print("socket is listening") 
-
-	# # a forever loop until client wants to exit 
-	# while True: 
-
-	# 	# establish connection with client 
-	# 	c, addr = s.accept() 
-
-	# 	# lock acquired by client 
-	# 	# print_lock.acquire() 
-	# 	print('Connected to :', addr[0], ':', addr[1]) 
-
-	# 	# Start a new thread and return its identifier 
-	# 	start_new_thread(threaded, (c,)) 
-	# s.close() 
-
-
 if __name__ == '__main__': 
 	Main() 
