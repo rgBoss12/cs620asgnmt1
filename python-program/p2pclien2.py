51e3b246b23291e972cfa6b6219237f94652ac4f
Rohan Garg <rohangarg12@gmail.com>
2019-09-13 23:23:47 +0530

    Working seed - recieving node list in client

--- python-program/p2pclien2.py
+++ python-program/p2pclien2.py
@@ -16,19 +16,45 @@ def threaded(c):
 		data = data[::-1] 
 
 		c.send(data)
+	c.shutdown(socket.SHUT_RDWR)
 	c.close()
 
+def chunks(l, k):
+	for i in range(0, len(l)-1, k):
+		yield(l[i:i+k])
+
 def Main():
+	seed_ip = '127.0.0.1'
+	seed_port = 12357
+	seed_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	seed_s.connect((seed_ip, seed_port))
+	node_list = ''
+	while True:
+		data = seed_s.recv(1024)
+		if(not data):
+			break
+		node_list += str(data.decode('ascii'))
+	
+	# print(node_list)
+
+	node_array = list(chunks((node_list.split(':')), 2))
+
+	
+
+	print(node_array)
+
+	seed_s.shutdown(socket.SHUT_RDWR)
+	seed_s.close()
 	if os.fork() == 0:
 		host = '127.0.0.1'
 
-		port = 4567
+		port = 4570
 
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 		time.sleep(5)
 		s.connect((host, port))
-		message = "client2"[::-1]
+		message = "client1"[::-1]
 		while True:
 			s.send(message.encode('ascii'))
 
@@ -40,11 +66,12 @@ def Main():
 					continue
 			else:
 					break
+		s.shutdown(socket.SHUT_RDWR)
 		s.close()
 		os._exit(0)
 	else:
 		host = ""
-		port = 7890
+		port = 7900
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.bind((host, port))
 
@@ -58,6 +85,7 @@ def Main():
 			print('Connected to :', addr[0], ':', addr[1])
 
 			start_new_thread(threaded, (c,))
+		s.shutdown(socket.SHUT_RDWR)
 		s.close()
 	
 if __name__ == '__main__': 
