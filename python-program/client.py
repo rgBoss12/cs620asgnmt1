079268ba7c16fcf0a47542cca4490e96d86b42db
Rohan Garg <rohangarg12@gmail.com>
2019-09-15 21:00:12 +0530

    Working two way, with message hashing and gossip protocol

--- python-program/client.py
+++ python-program/client.py
@@ -3,26 +3,56 @@ from _thread import *
 import threading
 import os
 import time
+from datetime import datetime
 import random
+import hashlib
 
+## common sockets array used by all threads
 lock = threading._RLock()
 socks = []
-# neighbours = []
+message_list = []
 listening_port = 0
-#used for recieving from each connection -- #TODO implement gossip protocol - on recieving message, check in ML and forward to neighbours(except from where recieived) only if not present
+
+## used for recieving from each connection -- implemented gossip protocol - on recieving message, check in ML and forward to neighbours(except from where recieived) only if not present
 def threaded(c):
+	global message_list
 	while True: 
-		print("hello")
+		# print("data recvd")
 		data = c.recv(1024) 
 		if not data: 
-			print('Bye') 
 			break
-		print(data.decode('ascii')[::-1])
-		# data = data[::-1] 
+		
+		hash_object = hashlib.sha1(data)
+		hex_dig = hash_object.hexdigest()
+		# print("hex gentd", hex_dig)
 
-		# c.send(data)
-	# c.shutdown(socket.SHUT_RDWR)
-	c.close()
+		## if check_ml stays 0, this means that this message hasn't been seen previously
+		check_ml = 0
+		for i in message_list:
+			if i==hex_dig:
+				check_ml = 1
+				break
+		
+		if check_ml ==0:
+			f = open("outputfile.txt", "a")
+			print(data.decode('ascii'))
+			f.write(data.decode('ascii'))
+			f.close()
+			message_list.append(hex_dig)
+			children = []
+			for sock in socks:
+				if sock==c:
+					continue
+				child = 0
+				if not os.fork():
+					child = os.getpid()
+					grandchild_to_send(sock)
+					os._exit(0)
+				else:
+					children.append(child)
+					continue
+				
+	# c.close()
 
 def listen_th():
 	global listening_port
@@ -31,49 +61,41 @@ def listen_th():
 	port = listening_port
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((host, port))
-
-	print("listening socket binded to port", port)
-
 	s.listen(5)
-	print("socket is listening for incoming connections")
 	while True:
+		## accept incoming connections
+		
 		c, addr = s.accept()
-		print('Start recieving from :', addr[0] + ':' + str(addr[1]))
-
+		# print("cnct_acc")
 		## increase in neighbours : to start sending message to them as well
 		lock.acquire()
 		socks.append(c)
-		
-		# print(neighbours)
 		lock.release()
 		# start_new_thread(threaded, (c,))
 		thread = threading.Thread(target=threaded, args=(c,))
 		thread.start()
-	s.close()
+	# s.close()
 
 #function to split an array into even chunks
 def chunks(l, k):
 	for i in range(0, len(l)-1, k):
 		yield(l[i:i+k])
 
-def grandchild_to_send(sock, node):
+def grandchild_to_send(sock):
 	global listening_port
-	# host = node[0]
-	# port = int(node[1])
-	# print(node)
-	# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	## start recieving from new made connection
 	thread1 = threading.Thread(target=threaded, args=(sock,))
 	thread1.start()
-	# s.connect((host, port))
-	# # print("connected to " + node[0]+':'+node[1])
-	message = ("client at " + str(sock.getsockname()[1]))[::-1]
-	# s.send(message.encode('ascii'))
-	# for i in range(10):
+	
+
+	dateTimeObj = datetime.now()
+	timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+	#message = ""+timestampStr+":"+sock.getsockname()[0]+":message"
+	message = "client at "+sock.getsockname()[0]+":"+str(sock.getsockname()[1])
+	## send generated message
 	sock.send(message.encode('ascii'))
-	print("Message sent to", str(sock.getpeername()[1]))
-		# time.sleep(5)
-	# s.shutdown(socket.SHUT_RDWR)
-	# s.close()
+	
 
 def Main():
 	global socks
@@ -81,7 +103,7 @@ def Main():
 
 	## connects to the seed
 	seed_ip = '127.0.0.1'
-	seed_port = 12395
+	seed_port = 12456
 
 	seed_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	seed_s.connect((seed_ip, seed_port))
@@ -97,73 +119,43 @@ def Main():
 		node_list += str(data.decode('ascii'))
 	
 	node_array = list(chunks((node_list.split(':')), 2))
-	print(node_array)
-
+	f = open("outputfile.txt", "a")
+	print(str(node_array))
+	
 	## pick your neighbours
-	# lock.acquire()
 	neighbours = random.choices(node_array, k=random.choice(list(range(1,min(5, len(node_array)+1))) if len(node_array)>0 else [0]))
 
-	print(neighbours)
-	# lock.release()
 	## connection with seed closed
 	seed_s.close()
-	# children = []
-
-	# child = os.fork()        
-	## parent process (this one) starts listening for incoming connections from other clients 
 	
+	## thread to start listening on the incoming active connections
 	thread = threading.Thread(target=listen_th, args=())
 	thread.start()
 
-	# else:
-	# socks = []
-	# lock.acquire()
-	# n1 = neighbours
-	# lock.release()
+	## iterate on neighbours selected above to create sockets list
 	for node in neighbours:
 		host = node[0]
 		port = int(node[1])
-		print(node)
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((host, port))
 		socks.append(s)
-	old_length = len(neighbours)
-	children = []
-	for i in range(10):
-		# print(neighbours)
-		# lock.acquire()
-		# n2 = neighbours
-		# lock.release()
-		# print(len(n2))
-		# for node in n2[old_length:]:
-		# 	host = node[0]
-		# 	port = int(node[1])
-		# 	print(node)
-		# 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		# 	s.connect((host, port))
-		# 	# print("connected to " + node[0]+':'+node[1])
-		# 	# message = ("client at " + str(listening_port))[::-1]
-		# 	socks.append(s)
-		indx = 0
+	children = []
+
+	## generate messages once every 5
+	for i in range(10):
 		for sock in socks:
-			indx += 1
 			child = 0
-			print(indx)
 			if not os.fork():
 				child = os.getpid()
-				grandchild_to_send(sock, indx)
+				grandchild_to_send(sock)
 				os._exit(0)
 			else:
 				children.append(child)
 				continue
-		# old_length = len(n2)
 		time.sleep(5)
 	for child in children:
 		os.waitpid(child, 0)
-	for sock in socks:
-		sock.close()
-		# os._exit(0)
 
 if __name__ == '__main__': 
 	Main() 
--- python-program/seed.py
+++ python-program/seed.py
@@ -2,15 +2,15 @@ import socket
 from array import *          
 
 s = socket.socket()      
-print("Socket successfully created")
+# print("Socket successfully created")
 
-port = 12395
+port = 12456
 
 s.bind(('', port))       
-print("socket binded to %s" %(port))
+# print("socket binded to %s" %(port))
 
 s.listen(5)  
-print("socket is listening")
+# print("socket is listening")
 
 CL=[[]]
 
@@ -25,11 +25,11 @@ while True:
 			str1 = str1 + j + ":"
 
 	# str1 = str1.join(i)
-	print(str1)
+	# print(str1)
 	c.send(str1.encode('ascii'))
 	
 	CL.append([addr[0],str(addr[1])])
-	print(CL)
+	# print(CL)
 	# c.shutdown(socket.SHUT_RDWR)
 	c.close() 
 
